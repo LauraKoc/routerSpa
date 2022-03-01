fb89cbe9a9d81786fa858d982502c59af7af7f06
Laura <40889285+LauraKoc@users.noreply.github.com>
2022-03-01 09:38:57 +0200

    add add

--- js/router.js
+++ js/router.js
@@ -1,21 +1,26 @@
+import { sayHi, sayBye } from "./app.js";
+
+sayHi("John"); // Hello, John!
+sayBye("John"); // Bye, John!
+
 const route = (event) => {
-    event = event || window.event;
-    event.preventDefault();
-    window.history.pushState({}, "", event.target.href);
-    handleLocation();
+  event = event || window.event;
+  event.preventDefault();
+  window.history.pushState({}, "", event.target.href);
+  handleLocation();
 };
 
 const routes = {
-    404: "/pages/404.html",
-    "/": "/pages/index.html",
-    "/about": "/pages/about.html"
+  404: "/pages/404.html",
+  "/": "/pages/index.html",
+  "/about": "/pages/about.html"
 };
 
 const handleLocation = async () => {
-    const path = window.location.pathname;
-    const route = routes[path] || routes[404];
-    const html = await fetch(route).then((data) => data.text());
-    document.getElementById("main-page").innerHTML = html;
+  const path = window.location.pathname;
+  const route = routes[path] || routes[404];
+  const html = await fetch(route).then((data) => data.text());
+  document.getElementById("main-page").innerHTML = html;
 };
 
 window.onpopstate = handleLocation;
